76c6feb0a81eb58f11bdbb63c819232fe04bfc6d
Alan Perez <alanp9six@gmail.com>
2019-10-15 12:20:46 -0500

    following class lecture

---
+++ text_buffer/text_buffer.py
@@ -0,0 +1,21 @@
+
+
+class TextBuffer:
+    def _init_(self, init=none):
+        pass
+
+    def _str_(self):
+        # Printthe contetns of buffer
+        pass
+
+    def append(self, string_to_add):
+        pass
+
+    def prepend(self, string_to_add):
+        pass
+
+    def delete_front(self, chars_to_remove):
+        pass
+
+    def delete_back(self, chars_to_remove):
+        pass
